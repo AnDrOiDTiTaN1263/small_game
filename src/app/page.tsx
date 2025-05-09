7ceb1d6ee9c9926e867dd7092b8e26339f457c11
s3949273 <s3949273@student.rmit.edu.au>
2025-05-10 09:09:19 +1000

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="grid grid-flow-row w-full h-full">
-        New investing game
+        <div className="grid row-span-2 w-full col-span-full justify-center">
+        Hello Navbar
+        </div>
 
     </div>
   );
