c85cd093ec62bb096bca7b52d4ce3414b02ec57b
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2025-05-10 09:11:56 +1000

    added extra styling and main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-flow-row w-full h-full">
-        <div className="grid row-span-2 w-full col-span-full justify-center">
+    <div className="grid grid-flow-row grid-rows-12 w-screen h-screen gap-2">
+        <div className="grid row-span-1 w-full col-span-full justify-center bg-slate-300">
         Hello Navbar
         </div>
+        <div className="grid row-span-11 w-full bg-slate-300">
 
+
+        </div>
     </div>
   );
 }
