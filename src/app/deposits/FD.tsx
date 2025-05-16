b1e737e9fc7676e95e0c379724f226a0806ef3a2
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2025-05-16 21:42:00 +1000

    moved and renamed fixed deposit component

--- src/app/deposits/FD.tsx
+++ src/app/deposits/FD.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Deposit } from '../constants'
 
 
@@ -7,6 +7,7 @@ export default function DepositComponent({deposit, id, setFixedDeposits, setBala
         setBalance(prev=>prev+deposit.depositAmount);
         setFixedDeposits(prev=>prev.filter((_,i)=>i!==id));
     }
+    const [addDeposit, setAddDepoist] = useState(false);
     return (
     <div className='grid grid-flow-row-dense w-full h-20 px-2 border-2 rounded-lg'>
         <div className='flex  gap-10'>
@@ -19,9 +20,17 @@ export default function DepositComponent({deposit, id, setFixedDeposits, setBala
             </div>
             
         </div>
-        <div>
-                <button onClick={handleWithdraw} className='border-2 px-2 h-8 rounded-lg hover:bg-black hover:text-white'>Withdraw</button>
-            </div>
+        <div className='flex gap-2'>
+            <button onClick={()=>{setAddDepoist(!addDeposit)}} className='border-2 px-2 h-8 rounded-lg hover:bg-black hover:text-white'>{addDeposit?"Cancel":"Deposit"}</button>
+            {
+                addDeposit &&
+                <div className='flex gap-2'>
+                    <input type='number' className=' px-1 border-2 outline-none h-8 rounded-lg'/>
+                    <button className='border-2 px-2 h-8 rounded-lg hover:bg-black hover:text-white'>Confirm</button>
+                </div>
+            }
+            <button onClick={handleWithdraw} className='border-2 px-2 h-8 rounded-lg hover:bg-black hover:text-white'>Withdraw</button>
+        </div>
     </div>
   )
 }
